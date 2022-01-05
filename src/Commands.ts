cc2744b71a2b2b344f138186c9e42335f306130e
ByWezZ <46261050+ByWezZ@users.noreply.github.com>
2022-01-05 21:55:17 +0100

    Command Interface

---
+++ src/Commands.ts
@@ -0,0 +1,11 @@
+import { Client, Message } from "discord.js";
+
+export interface Command {
+    names: Array<String>;
+    description: String;
+
+    admin?: boolean; // should this command be admin only ?
+    hide?: boolean; // Should this command apear in help command ?
+
+    run: (client: Client, message: Message, args: String[]) => void;
+}
