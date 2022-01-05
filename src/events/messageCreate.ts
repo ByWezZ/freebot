78b8b15f3af8f6e3747faf014bcaa15999c8d7b9
ByWezZ <46261050+ByWezZ@users.noreply.github.com>
2022-01-06 00:11:16 +0100

    Command handler event

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -1,3 +1,24 @@
-import { Client, Message } from "discord.js";
+import { Message } from "discord.js";
+import { SuperClient } from "../index";
+import { prefix } from "../config.json";
 
-export function run(client: Client, message: Message): void {}
+export function run(client: SuperClient, message: Message): void {
+    // Command handling
+    if (!message.content.startsWith(prefix)) return;
+    message.content = message.content.slice(prefix.length);
+
+    let args = message.content.split(/ +/);
+    let commandName = args.shift() ?? "";
+
+    let command = client.commands.cache.find((_, names) =>
+        names.includes(commandName)
+    );
+
+    if (
+        !command ||
+        (command.admin && !message.member?.permissions.has("ADMINISTRATOR"))
+    )
+        return;
+
+    command.run(client, message, args, commandName);
+}
