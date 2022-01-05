3919eb06c1ddb23fff8512a0b28e460a80d957cb
ByWezZ <46261050+ByWezZ@users.noreply.github.com>
2022-01-05 20:41:22 +0100

    Launch the bot

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,50 @@
-import Discord from "discord.js";
+import { Client, ClientOptions, User } from "discord.js";
+import { promises as fs } from "fs";
 import config from "./config.json";
 
-const client = new Discord.Client({
+import { Command } from "./Commands";
+
+interface CommandManager {
+    cache: Map<String[], Command>;
+    members: Map<User, Command>;
+}
+
+class SuperClient extends Client {
+    commands: CommandManager;
+
+    constructor(options: ClientOptions) {
+        super(options);
+
+        this.commands = {
+            cache: new Map(),
+            members: new Map(),
+        };
+    }
+}
+
+const client: SuperClient = new SuperClient({
     intents: ["DIRECT_MESSAGES", "GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES"],
 });
 
-client.login(config.token);
+/**
+ * Handling events and commands
+ */
+client.on("ready", async () => {
+    let commands = await fs.readdir(__dirname + "/commands");
+
+    for (let commandFile of commands) {
+        let commandInfo: Command = require(`${__dirname}/commands/${commandFile}`);
+
+        client.commands.cache.set(commandInfo.names, commandInfo);
+    }
+
+    let events = await fs.readdir(__dirname + "/events");
+
+    for (let eventFile of events) {
+        let eventInfo = require(`${__dirname}/events/${eventFile}`);
+
+        client.on(eventFile.split(".")[0], eventInfo.run.bind(null, client));
+    }
+});
+
+client.login(process.env.FREEBOT || config.token);
