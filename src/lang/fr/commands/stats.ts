fc3479ebeda57be2b0c77ace70b974e6906cb00f
ByWezZ <valentin_clm@outlook.fr>
2022-04-02 20:01:36 +0200

    feat(lang): Added stats langages

---
+++ src/lang/fr/commands/stats.ts
@@ -0,0 +1,10 @@
+export default {
+    embed: {
+        title: "Statisique de {{botName}}",
+        description:
+            "__**Nombre de serveurs :**__ {{guilds}}\n__**Nombre de membres :**__ {{members}}\n\n*Informations sur les différents shards*",
+        fields: {
+            value: "Serveurs: {{guilds}}\nMembres: {{members}}",
+        },
+    },
+};
