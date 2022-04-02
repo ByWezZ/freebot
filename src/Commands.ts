31e4eb30cb9b7eb986ad7514f91bac4d7d01b0eb
ByWezZ <valentin_clm@outlook.fr>
2022-04-02 11:29:22 +0200

    minor(Command): Grammar

--- src/Commands.ts
+++ src/Commands.ts
@@ -3,5 +3,5 @@ import { ApplicationCommandData, Client, CommandInteraction } from "discord.js";
 export interface Command {
     infos: ApplicationCommandData;
 
-    run: (client: Client, message: CommandInteraction) => void;
+    run: (client: Client, interaction: CommandInteraction) => void;
 }
