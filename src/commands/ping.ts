78dab7ed2d1ac54823c7d9ec946d5b1f9251e43a
ByWezZ <valentin_clm@outlook.fr>
2022-04-02 12:02:06 +0200

    fix(ping): Updated name

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -2,9 +2,11 @@ import { Command } from "../Command";
 
 export const command: Command = {
     infos: {
-        name: "testCommand",
+        name: "test-command",
         description: "Used to test some of the bot's fonctionnalities",
     },
+
+    guildId: "598814767771156490",
 
     run: (client, commandInt) => {
         commandInt.reply("pong");
