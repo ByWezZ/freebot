99cdf9714e8d854a54a1b8794176bc19ea864ac1
ByWezZ <valentin_clm@outlook.fr>
2022-04-02 11:44:30 +0200

    feat(Command): Added appCommand property

--- src/Command.ts
+++ src/Command.ts
@@ -1,8 +1,13 @@
-import { ApplicationCommandData, BaseCommandInteraction } from "discord.js";
+import {
+    ApplicationCommand,
+    ApplicationCommandData,
+    BaseCommandInteraction,
+} from "discord.js";
 import { SuperClient } from ".";
 
 export interface Command {
     infos: ApplicationCommandData;
+    appCommand?: ApplicationCommand;
 
     run: (client: SuperClient, interaction: BaseCommandInteraction) => void;
 }
